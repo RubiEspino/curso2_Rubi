00ea218b1adfe1305e8c117a82483e4f8233b384
RubiEspino <mary.rubi2000@gmail.com>
2026-08-25 22:13:10 +0000

    Implement Post management with CRUD operations and views

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
-
+use App\Http\Controllers\PostController;
 /*Route::get('/', function () {
     return view('welcome');
 });*/
@@ -17,9 +17,12 @@ use App\Models\Post;
     return view('portada', ['posts' => $posts]);
 });*/
 
-Route::get('/', function () {
-    $posts = Post::with('categoria')->latest()->get();
+Route::get('/', [PostController::class, 'index'])->name('avisos.index');
+Route::get('/avisos/crear', [PostController::class, 'create'])->name('avisos.create');
+Route::post('/avisos', [PostController::class, 'store'])->name('avisos.store');
+Route::get('/avisos/{post}/editar', [PostController::class, 'edit'])->name('avisos.edit');
+Route::put('/avisos/{post}', [PostController::class, 'update'])->name('avisos.update');
+Route::delete('/avisos/{post}', [PostController::class, 'destroy'])->name('avisos.destroy');
 
-    return view('portada', ['posts' => $posts]);
-});
+
 Route::get('/contacto', fn () => view('contacto'));
