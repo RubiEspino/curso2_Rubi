ae51bdb08e8b95221a18945e41521983bf35d1df
RubiEspino <mary.rubi2000@gmail.com>
2026-09-10 22:31:44 +0000

    Completa sesion 4 con Livewire y policies

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -12,10 +14,14 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+     public function canAccessPanel(Panel $panel): bool
+    {
+        return in_array($this->rol, ['admin', 'editor']);
+    }
 
     /**
      * Get the attributes that should be cast.
--- resources/views/portada.blade.php
+++ resources/views/portada.blade.php
@@ -15,9 +15,7 @@
 
     <div class="max-w-4xl mx-auto p-8">
         <div class="grid md:grid-cols-2 gap-4">
-            @foreach ($posts as $post)
-                <x-tarjeta-post :post="$post" />
-            @endforeach
+            <livewire:buscador-avisos />
         </div>
     </div>
 @endsection
